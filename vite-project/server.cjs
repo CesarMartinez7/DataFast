b55e9b74874dff74c97d244924e2f6c10a86aabf
CesarMartinez <cesarwamartinez@gmail.com>
2024-12-23 00:03:17 -0500

    add start

--- vite-project/server.cjs
+++ vite-project/server.cjs
@@ -13,76 +13,70 @@ app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-const dataServerDB = {
-  host: "localhost",
-  user: "root",
-  password: "oyasumipunpun",
-  database: "nodomobiliario",
-};
 
-const conec = mysql.createConnection(dataServerDB);
+// const conec = mysql.createConnection(dataServerDB);
 
-conec.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Conectado a la base de datos con éxito");
-});
+// conec.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Conectado a la base de datos con éxito");
+// });
 
 
-app.get("/api/datos", (req, resp,) => {
-  let table = req.query.table || "activo"
-  const sql = `SELECT * FROM ${table}`;
-  conec.query(sql, (err, resultadoQuery) => {
-    if (err) {
-      return resp.status(500).json({ error: "Error en la consulta" });
-    }
-    resp.json(resultadoQuery);
-  });
-});
+// app.get("/api/datos", (req, resp,) => {
+//   let table = req.query.table || "activo"
+//   const sql = `SELECT * FROM ${table}`;
+//   conec.query(sql, (err, resultadoQuery) => {
+//     if (err) {
+//       return resp.status(500).json({ error: "Error en la consulta" });
+//     }
+//     resp.json(resultadoQuery);
+//   });
+// });
 
 
-app.post("/registrer", (req, resp) => {
-  // Datos q vienen de el formulario
-  let name = req.body.name;
-  let email = req.body.email;
-  let password = req.body.password;
-  const data = [name, email, password];
+// app.post("/registrer", (req, resp) => {
+//   // Datos q vienen de el formulario
+//   let name = req.body.name;
+//   let email = req.body.email;
+//   let password = req.body.password;
+//   const data = [name, email, password];
 
-  conec.execute("SELECT count(nombre) as RESULTADO FROM userroot WHERE nombre = ? OR email = ?", [name, email], (err, resultado) => {
-    if (err) {
-      return resp.status(500).send("Error en la base de datos");
-    }
+//   conec.execute("SELECT count(nombre) as RESULTADO FROM userroot WHERE nombre = ? OR email = ?", [name, email], (err, resultado) => {
+//     if (err) {
+//       return resp.status(500).send("Error en la base de datos");
+//     }
 
-    const userExistente = resultado[0].RESULTADO;
+//     const userExistente = resultado[0].RESULTADO;
 
-    if (userExistente > 0) {
-      return resp.status(400).send("Ya se encuentra registrado");
-    } else {
-      const InsertInto = "INSERT INTO userroot (nombre, email, password) VALUES (?, ?, ?)";
-      conec.execute(InsertInto, data, (err, result) => {
-        if (err) {
-          return resp.status(500).send("Error en la inserción");
-        }
-        console.log(`El resultado en la inserción es ${result}`);
-        resp.status(201).send("Usuario registrado con éxito");
-      });
-    }
-  });
-});
+//     if (userExistente > 0) {
+//       return resp.status(400).send("Ya se encuentra registrado");
+//     } else {
+//       const InsertInto = "INSERT INTO userroot (nombre, email, password) VALUES (?, ?, ?)";
+//       conec.execute(InsertInto, data, (err, result) => {
+//         if (err) {
+//           return resp.status(500).send("Error en la inserción");
+//         }
+//         console.log(`El resultado en la inserción es ${result}`);
+//         resp.status(201).send("Usuario registrado con éxito");
+//       });
+//     }
+//   });
+// });
 
 
 
 
-app.post("/datos", (req, resp) => {
-  console.log(req.body);
-  resp.end();
-});
+// app.post("/datos", (req, resp) => {
+//   console.log(req.body);
+//   resp.end();
+// });
 
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
